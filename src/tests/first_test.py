3a68f69e42d3446bad3d0f0c9227b47634d05ea5
Anssi Leikas <128043951+anleik@users.noreply.github.com>
2023-04-06 19:37:49 +0300

    Test.

--- src/tests/first_test.py
+++ src/tests/first_test.py
@@ -6,7 +6,7 @@ from main import BackgroundObject
 
 class TestBackgroundObject(unittest.TestCase):
     def setUp(self):
-        self.bgo = BackgroundObject(1200, 260, 300, 100, 0.9, (135, 135, 255))
+        self.bgo = BackgroundObject(1200, 260, 300, 100, (135, 135, 255), 0.9)
 
     def test_init(self):
-        self.assertEqual(self.bgo.c, (135, 135, 255))
+        self.assertEqual(self.bgo.color, (135, 135, 255))
